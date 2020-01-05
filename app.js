091704b5bb857263d085b8ac2fbada781d848b13
melius413 <melius413@gmail.com>
2020-01-05 15:09:18 +0900

    add mysql moduel

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const app = express();
 const port = 3000;
 const host = '127.0.0.1';
 
+const mysql = require('mysql');
+
+// const conn = mysql.createConnection({ // 리미트가 없음
+//     // 다쓰면 해제해줘야한다. release
+//     // 안해주면 쌓인다. 계속 쌓이면 서버 문제생김
+//     host: 'localhost',
+//     user: 'root',
+//     password: '000000',
+//     port: 3307,
+//     database: 'node'
+//     // 동접자 처리안됨
+//     // 끊어져야 다른사람 접속가능
+// });
+
+const conn = mysql.createPool({ // 실무에서 많이씀
+    // 동접자(동시접속) 처리 가능
+    host: 'localhost',
+    user: 'root',
+    password: '000000',
+    port: 3307,
+    database: 'node',
+    connectionLimit: 10
+});
+
 app.listen(port, () => {
     console.log(`http://${host}:${port}`);
 });
@@ -25,10 +49,27 @@ app.get(['/pug', '/pug/:page'], (req, res) => {
     switch (page) {
         case "list":
             vals.title = "게시글 리스트 입니다.";
-            vals.lists = [
-                {id:1, title: "첫번째 글", writer: "관리자", wdate: "2020-01-03", rnum: 5},
-                {id:2, title: "두번째 글", writer: "관리자2", wdate: "2020-01-04", rnum: 6},
-                {id:3, title: "세번째 글", writer: "관리자3", wdate: "2020-01-05", rnum: 4}
+            vals.lists = [{
+                    id: 1,
+                    title: "첫번째 글",
+                    writer: "관리자",
+                    wdate: "2020-01-03",
+                    rnum: 5
+                },
+                {
+                    id: 2,
+                    title: "두번째 글",
+                    writer: "관리자2",
+                    wdate: "2020-01-04",
+                    rnum: 6
+                },
+                {
+                    id: 3,
+                    title: "세번째 글",
+                    writer: "관리자3",
+                    wdate: "2020-01-05",
+                    rnum: 4
+                }
             ];
             res.render("list.pug", vals);
             // res.render("layout/list.pug", vals); // 경로라면 이렇게??
@@ -41,4 +82,21 @@ app.get(['/pug', '/pug/:page'], (req, res) => {
             res.redirect('/'); // location.href랑 같음
             break;
     }
+});
+
+app.get("/sqltest", (req, res) => {
+    let connect = conn.getConnection((err, connect) => { // connectionLimit: 10 개 중에 하나 를 connect로 줌
+        if (err) {
+            res.send("Database 접속에 실패했습니다.");
+        } else {
+            let sql = 'INSERT INTO board SET title="테스트입니까?", writer="관리자", wdate="2020-01-05 14:55:00"';
+            connect.query(sql, (err, result) => {
+                if (err) {
+                    res.send("SQL문이 실패하였습니다.");
+                } else {
+                    res.json(result);
+                }
+            });
+        }
+    });
 });
